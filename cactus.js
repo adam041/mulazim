47ef163b78484c9cecb3ff2e10e99a6ccfb256fb
adam041 <adam.kotkin11@gmail.com>
2018-02-13 17:14:04 -0500

    menu control implemented
    also removed “crap” subfolder of no longer used files

--- cactus.js
+++ cactus.js
@@ -1,30 +1,51 @@
 /*
 Work list
--insert jQuery menu to drive arRoot input
--breakout update scripts from .ready to own function
 
--pull jQuery menu inputs (from google sheet?)
--table styling & sizing cleanup
+- display active root & re-work table layout
+
+- styling
+    - three color scheme for rows (1 4 7 10, 2 5 8, 3 6 9
+    - noun / verb / both jQuery tabs
+    - row size cleanup
+    - fewer borders
+    - distinct header formats
+
+-core logic
+    -scripts for other columns verbs(3), nouns(3)
+    -handling irregular verbs
+
+-backend / data quality
+    -get more verbs n=5 >>> n=50 >>> n=500
+    -pull jQuery menu inputs (from google sheet?)
+    -handling of prepositions related to verb forms
+
+-secondary features
+    -within tense conjugation (i.e. for all subjects; add column on left)
+    -translation by mouseover or appended column
 */
 
 
 jQuery( document ).ready(function() {
 
-// Load Menu Control
+// Load Menu Control from backend
+// ** tbd
+
 
 //Implement click controls for menu
-$( "#clickBait" ).click(function() {
-      $( this ).html("yuo have been clicked");
+    $( "li" ).click(function() {
+        conjugateUpdate( $(this).text() );
     });
 
-//////////////////////////////////////////////////
-// Break Out Conjugation Update to Own Function **
+});
 
-    var strInput = "عمل";   //** eventually need to pull strInput from backend or 'menu' element
-    arRoot.root = strInput;
 
-    document.title = "LtCactus Conjugates " + strInput;
- //   jQuery("#rootTitle").html(strInput);
+function conjugateUpdate( root ) {
+//Updates data tables with conjugated verbs/nouns
+
+    arRoot.root = root;
+
+    document.title = "LtCactus Conjugates " + arRoot.root;
+    //*** update element giving user feedback as to which root was selected
 
     var htmlTableStart = "<table> <tbody> ",
         htmlTableRows = "",
@@ -69,9 +90,7 @@ $( "#clickBait" ).click(function() {
         }
     jQuery("#colActivePast").html( htmlTableStart + htmlTableRows + htmlTableEnd );
 
-//////////////////////////////////////////////////
-
-});
+};
 
 
 $( function() {
