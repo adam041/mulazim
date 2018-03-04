2f3d144229350c18c98840d83b72dadebec59edb
adam041 <adam.kotkin11@gmail.com>
2018-03-04 00:19:19 -0500

    began work on verbalize master/wrapper function
    Intend to retain conjActivePresent/conjPassivePast/etc for conjugating
    stem, while using verbalize to also conjugate subject-specific prefix
    and suffix…needs testing and handling of irregulars

--- constants.js
+++ constants.js
@@ -20,6 +20,8 @@ ar_am="ى";          //alif maqsura
 ar_U="و";           //wuuw
 
 //consonants
+ar_hHat = "ح";
+ar_hEye = "ه";
 ar_l="ل";           //lam
 ar_m="م";           //mim
 ar_n="ن";           //nuun
@@ -33,3 +35,202 @@ ar_h5="ء";          //hamza
 ar_mu="مٌ";         //mim + damma
 ar_Do="فعل";        //verb for debugging
 
+//pronouns - no short vowels except to distinguish you-singular
+pro_i = ar_hA + ar_n + ar_A;
+pro_we = ar_n + ar_hHat + ar_n;
+
+pro_youM = ar_hA + ar_n + ar_t + ar_a;
+pro_youF = ar_hA + ar_n + ar_t + ar_i;
+pro_vousM = ar_hA + ar_n + ar_t + ar_m;
+pro_vousF = ar_hA + ar_n + ar_t + ar_n;
+
+pro_he = ar_hEye + ar_U;
+pro_she = ar_hEye + ar_Y;
+pro_theyM = ar_hEye + ar_m;
+pro_theyF = ar_hEye + ar_n + ar_A;
+
+
+function prefixVowel(formNum, isActive) {
+//returns vowel accompanying prefix of present stem of verb
+
+if ( isActive === undefined ) {
+    isActive = true;
+}
+
+if ( isActive === true ) {
+    switch (formNum) {
+
+        case 2:
+        case 3:
+        case 4:
+            return ar_u;
+
+        case 1:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            return ar_a;
+
+        default:
+          return "؟";
+    }
+} else {
+    return ar_u;
+}
+
+}
+
+
+//new master function?
+function conjugate(root, formNum, tense, isActive, subject) {
+//finds stem of a verb, and then adds prefixes and suffixes appropriate to tense and subject
+
+//root and formNum are required parameters.  Active/Present/3rd person masculine single assumed if no param passed
+// tense parameter should be "present" or "past"
+// subject parameter should be lower case english*, with M/F as last character for you/vous */they
+
+//ensure optional variables are not null
+if ( tense === undefined ) {
+    tense = "present";
+}
+
+if ( isActive === undefined ) {
+    isActive = true;
+}
+
+if ( subject === undefined ) {
+    subject = "هو";
+}
+
+//Declare internal string variables
+var stem = "";
+var prefix = "";
+var suffix = "";
+
+//Get the stem
+
+if ( tense === "present" ) {
+
+    if ( isActive ) {
+        stem = conjActivePresent(root, formNum);
+    } else {
+        stem = conjPassivePresent(root, formNum);
+    }
+
+} else if ( tense === "past" ) {
+
+    if ( isActive ) {
+        stem = conjActivePast(root, formNum);
+    } else {
+        stem = conjPassivePast(root, formNum);
+    }
+
+} else if ( tense === "imperative" ) {
+    stem = conjImperative(root, formNum);
+}
+
+//Get Present Prefix/Suffix
+if ( tense === "present" ) {
+
+    //Present Prefixes
+    switch ( subject ) {
+
+        case pro_i:
+            prefix = ar_hA + prefixVowel(formNum, isActive);
+            break;
+
+        case pro_we:
+            prefix = ar_n + prefixVowel(formNum, isActive);
+            break;
+
+        case pro_youM:
+        case pro_youF:
+        case pro_she:
+        case pro_vousM:
+        case pro_vousF:
+            prefix = ar_t + prefixVowel(formNum, isActive);
+            break;
+
+        case pro_he:
+        case pro_theyM:
+        case pro_theyF:
+            prefix = ar_y + prefixVowel(formNum, isActive);
+            break;
+    }
+
+    //Present Suffixes
+    switch ( subject ) {
+
+        case pro_youF:
+        case pro_theyM:
+            suffix = ar_u + ar_U + ar_n;
+            break;
+
+        case pro_vousM:
+            suffix = ar_u + ar_U + ar_n + ar_a;
+            break;
+
+        case pro_vousF:
+        case pro_theyF:
+            suffix = ar_0 + ar_n + ar_a;
+            break;
+    }
+
+} else if ( tense === "past" ) {
+//Get Past Suffix
+
+    switch ( subject ) {
+
+        case pro_i:
+            suffix = ar_0 + ar_t + ar_u;
+            break;
+
+        case pro_we:
+            suffix = ar_0 + ar_n + ar_a;
+            break;
+
+        case pro_youM:
+            suffix = ar_0 + ar_t + ar_a;
+            break;
+
+        case pro_youF:
+            suffix = ar_0 + ar_t + ar_i;
+            break;
+
+        case pro_vousM:
+            suffix = "؟؟؟*";
+            break;
+
+        case pro_vousF:
+            suffix = "؟؟؟*";
+            break;
+
+        case pro_he:
+            suffix = ar_a;
+            break;
+
+        case pro_she:
+            suffix = ar_a + ar_t;
+            break;
+
+        case pro_theyM:
+            suffix = ar_u + ar_U + ar_A;
+            break;
+
+        case pro_theyF:
+            suffix = "؟؟؟*";
+            break;
+    }
+
+} else {
+// imperative Or fail
+}
+
+//check if irregular
+
+return prefix + stem + suffix;
+
+}
--- verbs.js
+++ verbs.js
@@ -1,5 +1,192 @@
 //Verb Conjugation Scripts
 
+//* new master function?
+function verbalize(root, formNum, tense, isActive, subject) {
+//finds stem of a verb, and then adds prefixes and suffixes appropriate to tense and subject
+
+//root and formNum are required parameters.  Active/Present/3rd person masculine single assumed if no param passed
+// tense parameter should be "present" or "past"
+// subject parameter should be lower case english*, with M/F as last character for you/vous */they
+
+//ensure optional variables are not null
+if ( tense === undefined ) {
+    tense = "present";
+}
+
+if ( isActive === undefined ) {
+    isActive = true;
+}
+
+if ( subject === undefined ) {
+    subject = "هو";
+}
+
+//Declare internal string variables
+var stem = "";
+var prefix = "";
+var suffix = "";
+var objRefs = makeReferenceObject();
+
+
+//Get the stem
+
+if ( tense === "present" ) {
+
+    if ( isActive ) {
+        stem = conjActivePresent(root, formNum, objRefs);
+    } else {
+        stem = conjPassivePresent(root, formNum, objRefs);
+    }
+
+} else if ( tense === "past" ) {
+
+    if ( isActive ) {
+        stem = conjActivePast(root, formNum, objRefs);
+    } else {
+        stem = conjPassivePast(root, formNum, objRefs);
+    }
+
+} else if ( tense === "imperative" ) {
+    stem = conjImperative(root, formNum);
+}
+
+//Get Present Prefix/Suffix
+if ( tense === "present" ) {
+
+    //Present Prefixes
+    switch ( subject ) {
+
+        case pro_i:
+            prefix = ar_hA + prefixVowel(formNum, isActive);
+            break;
+
+        case pro_we:
+            prefix = ar_n + prefixVowel(formNum, isActive);
+            break;
+
+        case pro_youM:
+        case pro_youF:
+        case pro_she:
+        case pro_vousM:
+        case pro_vousF:
+            prefix = ar_t + prefixVowel(formNum, isActive);
+            break;
+
+        case pro_he:
+        case pro_theyM:
+        case pro_theyF:
+            prefix = ar_Y + prefixVowel(formNum, isActive);
+            break;
+    }
+
+    //Present Suffixes
+    switch ( subject ) {
+
+        case pro_youF:
+        case pro_theyM:
+            suffix = ar_u + ar_U + ar_n;
+            break;
+
+        case pro_vousM:
+            suffix = ar_u + ar_U + ar_n + ar_a;
+            break;
+
+        case pro_vousF:
+        case pro_theyF:
+            suffix = ar_0 + ar_n + ar_a;
+            break;
+    }
+
+} else if ( tense === "past" ) {
+//Get Past Suffix
+
+    switch ( subject ) {
+
+        case pro_i:
+            suffix = ar_0 + ar_t + ar_u;
+            break;
+
+        case pro_we:
+            suffix = ar_0 + ar_n + ar_a;
+            break;
+
+        case pro_youM:
+            suffix = ar_0 + ar_t + ar_a;
+            break;
+
+        case pro_youF:
+            suffix = ar_0 + ar_t + ar_i;
+            break;
+
+        case pro_vousM:
+            suffix = "؟؟؟*";
+            break;
+
+        case pro_vousF:
+            suffix = "؟؟؟*";
+            break;
+
+        case pro_he:
+            suffix = ar_a;
+            break;
+
+        case pro_she:
+            suffix = ar_a + ar_t;
+            break;
+
+        case pro_theyM:
+            suffix = ar_u + ar_U + ar_A;
+            break;
+
+        case pro_theyF:
+            suffix = "؟؟؟*";
+            break;
+    }
+
+} else {
+// imperative Or fail
+}
+
+//check if irregular
+
+return prefix + stem + suffix;
+}
+
+
+function prefixVowel(formNum, isActive) {
+//returns vowel accompanying prefix of present stem of verb
+
+if ( isActive === undefined ) {
+    isActive = true;
+}
+
+if ( isActive === true ) {
+    switch (formNum) {
+
+        case 2:
+        case 3:
+        case 4:
+            return ar_u;
+
+        case 1:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            return ar_a;
+
+        default:
+          return "؟";
+    }
+} else {
+    return ar_u;
+}
+
+}
+
+
 function conjActivePast(root, formNum, objRefs) {
 //returns conjugated trilateral verb in Past Perfect (active)
 
@@ -10,19 +197,19 @@ function conjActivePast(root, formNum, objRefs) {
 
         case 1:
           rad2vowel = vowelMe(objRefs.query(root, formNum, "f1ActivePastRad2"));
-          output = root[0] + ar_a + root[1] + rad2vowel + root[2] + ar_a;
+          output = root[0] + ar_a + root[1] + rad2vowel + root[2];
           break;
 
         case 2:
-          output = root[0] + ar_a + root[1] + ar_2v + ar_a + root[2] + ar_a;
+          output = root[0] + ar_a + root[1] + ar_2v + ar_a + root[2];
           break;
 
         case 3:
-          output = root[0] + ar_a + ar_A + root[1] + ar_a + root[2] + ar_a;
+          output = root[0] + ar_a + ar_A + root[1] + ar_a + root[2];
           break;
 
         case 4:
-          output = ar_hA + ar_a + root[0] + ar_0 + root[1] + ar_a + root[2] + ar_a;
+          output = ar_hA + ar_a + root[0] + ar_0 + root[1] + ar_a + root[2];
           break;
 
         case 5:
@@ -34,19 +221,19 @@ function conjActivePast(root, formNum, objRefs) {
           break;
 
         case 7:
-          output = ar_A + ar_i + ar_n + ar_0 + root[0] + ar_a + root[1] + ar_a + root[2] + ar_a;
+          output = ar_A + ar_i + ar_n + ar_0 + root[0] + ar_a + root[1] + ar_a + root[2];
           break;
 
         case 8:
-          output = ar_A + ar_i + root[0] + ar_0 + ar_t + ar_a + root[1] + ar_a + root[2] + ar_a;
+          output = ar_A + ar_i + root[0] + ar_0 + ar_t + ar_a + root[1] + ar_a + root[2];
           break;
 
         case 9:
-          output = ar_A + ar_i + root[0] + ar_0 + root[1] + ar_a + root[2] + ar_2v+ ar_a;
+          output = ar_A + ar_i + root[0] + ar_0 + root[1] + ar_a + root[2] + ar_2v;
           break;
 
         case 10:
-          output = ar_A + ar_i + ar_s + ar_0 + ar_t + ar_a + root[0] + ar_0 + root[1] + ar_a + root[2] + ar_a;
+          output = ar_A + ar_i + ar_s + ar_0 + ar_t + ar_a + root[0] + ar_0 + root[1] + ar_a + root[2];
           break;
 
         default:
@@ -54,8 +241,11 @@ function conjActivePast(root, formNum, objRefs) {
           break;
       }
 
-      return pastIrregular(root, output, "he");
+        return pastIrregular(root, output, "he");
 }
+
+
+
 
 
 function conjActivePresent(root, formNum, objRefs) {
