45be4782f35f1ca2dc800de3644cbf72e8eb0c36
adam041 <adam.kotkin11@gmail.com>
2018-06-26 23:27:11 -0400

    made form-specific tables wider and shorter
    changed verb conjugation script from batching all 10 forms to only 1
    (but it doesn’t work)

--- multi.js
+++ multi.js
@@ -6,6 +6,20 @@ function readyPlayer2() {
         //for multicactus.html, not needed for cactus.html
     } );
 
+    $( function() {
+        $( ".fAccordion" ).accordion();
+        //for multicactus.html, not needed for cactus.html
+    } );
+
+    $( function() {
+        $( ".fAccordion" ).accordion();
+        //for multicactus.html, not needed for cactus.html
+    } );
+
+    $( function() {
+        $( "#draftTabs" ).tabs();
+    } );
+
     $( "#kickIt" ).button();
     $( "#kickIt" ).click( function( event ) {
         kicker(ar_Do); //* pull from select *
@@ -61,6 +75,12 @@ function kicker(arRoot) {
         fTable = $( "#fTableDraft" ).html().wrap("<table>");
         $( "#accordion .accFormDiv").eq(formNum - 1).html(fTable);
 
+        //re-init accordion
+//         $( function() {
+//             $( ".fAccordion" ).accordion();
+//             //for multicactus.html, not needed for cactus.html
+//         } );
+
     //loop to next form
     }
 
@@ -84,7 +104,8 @@ function sTable(arRoot, enTense, isActive, formNum) {
     var cnjdVerb = "";
 
     arrSubjects.forEach( function(value, index){
-        cnjdVerb = whole( cnjVerb( arRoot, enTense, isActive, eval("pro_" + value) )[formNum] );
+//         cnjdVerb = whole( cnjVerb( arRoot, enTense, isActive, eval("pro_" + value) )[formNum] );
+        cnjdVerb = whole(cnjVerb1( arRoot, enTense, isActive,  eval("pro_" + value), formNum ) );
         $( "#subTableDraft ." + value ).html(cnjdVerb);
     });
 
@@ -92,4 +113,35 @@ function sTable(arRoot, enTense, isActive, formNum) {
 //return draft
 subTable = $( "#subTableDraft" ).html();
 return subTable.wrap("<table>");
+}
+
+
+function cnjVerb1( arRoot, enTense, isActive,  arSubject, formNum ){
+//master function for verb conjugation
+
+var drafts = [],
+    draft = [];
+
+var word = new Word(arRoot, enTense, isActive, arSubject);
+
+//get base conj, for all 10 forms
+    draft = cnjForm( word, formNum );
+    drafts.push(draft);
+
+//get regular verb conj, for all 10 forms
+    draft = cnjRegularVerb(word);
+    drafts.push(draft);
+
+//adjust conj if irregular, for all 10 forms
+    draft = cnjIrregularVerb(word);
+    drafts.push(draft);
+
+
+//apply QA routines
+    draft = qaVerb(word);
+    drafts.push(draft);
+
+console.log(drafts);
+return drafts.last();
+
 }
